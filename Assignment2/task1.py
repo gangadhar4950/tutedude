85ea41c14f8fd44ae401f4f5fbf77e5b5517b5b2
Gangadhar Maregowda <gangadhar.maregowda@gmail.com>
2025-04-28 09:56:18 +0530

    Assignment
    Assignment

---
+++ Assignment2/task1.py
@@ -0,0 +1,5 @@
+x = int(input("Enter the number: "))
+if x % 2 == 0:
+    print(x, " is a Even number")
+else:
+    print(x, " is a Odd number")
---
+++ Assignment2/task2.py
@@ -0,0 +1,4 @@
+y = 0
+for i in range(1, 51):
+    y = y + i
+print(y)
